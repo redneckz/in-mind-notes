0381479c279521378da5678d6e35b9443f11b0a9
redneckz <aalexandrovv@gmail.com>
2015-12-08 21:30:54 +0300

    less support added

---
+++ grunt-config/grunt-task-less.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+  grunt.config.merge({
+    less: {
+      all: {
+        src: 'src/**/*.less',
+        dest: 'dist/css/in-mind-notes.css'
+      }
+    }
+  });
+};
